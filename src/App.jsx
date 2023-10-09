76c3efa05248710511bf16383e3f092fcb6631e2
sarwar-jahin-port <sarwarjahinpersonal@gmail.com>
2023-10-10 00:35:11 +0600

    basic functinalities added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [page, setPage] = useState(true);
+  const [print, setPrint] = useState(false);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -24,19 +26,19 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here (e.g., send data to a server or update state)
-    console.log('Products:', products);
-    // Reset the form fields
-    setProducts(products.map((product) => ({ ...product, quantity: 0 })));
+    setPrint(!print);
   };
 
   return (
     <>
-      <div className='grid grid-cols-3'>
-        <div className="left-width">
+      <div className='grid grid-cols-3 gap-20'>
+        <div className={`left-width ${print ? "hidden" : "block"}`}>
+          <div className='text-center'>
+            <button onClick={()=>setPage(!page)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Page Toggle</button>
+          </div>
           <h1 className='text-2xl text-center'>Products List</h1>
           <div className="products">
-            <form onSubmit={handleSubmit} className="space-y-2">
+            <form onSubmit={(handleSubmit)} className="space-y-2">
               {products.map((product) => (
                 <div key={product.id} className="flex items-center space-x-1">
                   <span className="w-1/2">{product.product_name}</span>
@@ -53,13 +55,28 @@ function App() {
                 type="submit"
                 className="w-full px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
               >
-                Submit
+                Print
               </button>
             </form>
           </div>
         </div>
-        <div className="right-width col-span-2">
-          <p>Right Width</p>
+        <div className={`right-width col-span-2 border ${page ? 'a4' : 'a5'} flex flex-wrap gap-2 pl-4`}>
+          {
+            products.map((product) =>
+              Array.from({ length: product.quantity }).map((_, index) => (
+                <>
+                  {product.img_link.map((img_link, imgIndex) => (
+                    <img
+                      className='w-[138px] h-[159px]'
+                      key={imgIndex}
+                      src={img_link}
+                      alt=""
+                    />
+                  ))}
+                </>
+              ))
+            )
+          }
         </div>
       </div>
     </>
